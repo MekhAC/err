2301fc3a2016f1ad9c2cbb6060501888f0a83433
MekhAC <apoorvchoudharyjaiswal13@gmail.com>
2026-03-03 15:38:28 +0530

    Removing lock

--- info.py
+++ info.py
@@ -1,9 +1,7 @@
 from collections import Counter, deque
-import threading
 
 class LogStore:
     def __init__(self):
         self.level_count = Counter()
         self.service_count = Counter()
         self.error_logs = deque()  # (timestamp, service, message)
-        self.lock = threading.Lock()
